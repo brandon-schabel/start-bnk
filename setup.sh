364d6607b6c57dff89ed0784bc71cd495d1e5b3e
brandon-schabel <brandonschabel1995@gmail.com>
2023-11-22 22:18:59 -0700

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,15 @@ command_exists() {
   type "$1" &> /dev/null
 }
 
+# Function to stop the server
+stop_server() {
+  echo "Stopping the server..."
+  kill "$server_pid" 2> /dev/null
+}
+
+# Trap EXIT signal to ensure server stops when script exits
+trap stop_server EXIT
+
 # Install Bun if not already installed
 if ! command_exists bun; then
   echo "Bun is not installed. Installing Bun..."
@@ -18,12 +27,13 @@ fi
 echo "Installing dependencies with bun install..."
 bun install
 
-# Start the server
+# Start the server and get its PID
 echo "Starting the server with bun dev..."
 bun dev &
+server_pid=$!
 
 # Wait for server to start
-sleep 5
+sleep 0.5
 
 # Open the web browser
 echo "Opening the browser at http://localhost:3000..."
@@ -35,8 +45,11 @@ else
   echo "Browser cannot be opened automatically. Please open localhost:3000 manually."
 fi
 
-echo "Setup complete!"
+echo "Setup complete! To stop the server, exit this script."
 
 echo "Start server again by running \"bun dev\" in the project directory."
 
 echo "View JSON route at http://localhost:3000/json"
+
+# Keep the script running until it receives a signal to exit
+wait $server_pid
